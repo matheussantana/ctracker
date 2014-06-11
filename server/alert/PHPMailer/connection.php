dd34ee95b2f680bb71195d1edae28a0b8d20853d
root <root@localhost.localdomain>
2014-06-10 22:17:57 -0400

    bla

--- server/alert/PHPMailer/connection.php
+++ server/alert/PHPMailer/connection.php
@@ -7,12 +7,12 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.yahoo.com';  // Specify main and backup SMTP servers
 $mail->Port='465';
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'asd2fd23afdv';                 // SMTP username
-$mail->Password = 'A@1abcde3';                           // SMTP password
+$mail->Username = 'xxxxxxxxx';                 // SMTP username
+$mail->Password = 'yyyyyyyyyyyyyy';                           // SMTP password
 $mail->SMTPSecure = 'ssl';                            // Enable encryption, 'ssl' also accepted
 //$mail->SMTPDebug = 1;
 
-$mail->From = 'asd2fd23afdv@yahoo.com.br';
+$mail->From = 'yyyyyyyyyyy';
 $mail->FromName = 'Ctracker';
 
 ?>

6366ef13bc3e377266f35a2878788a156ee0d26c
root <root@localhost.localdomain>
2014-06-14 23:57:05 -0400

    template email files

--- server/alert/PHPMailer/connection.php
+++ server/alert/PHPMailer/connection.php
@@ -4,15 +4,15 @@ require 'PHPMailerAutoload.php';
 $mail = new PHPMailer;
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.yahoo.com';  // Specify main and backup SMTP servers
+$mail->Host = 'br142.hostgator.com.br ';  // Specify main and backup SMTP servers
 $mail->Port='465';
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'xxxxxxxxx';                 // SMTP username
-$mail->Password = 'yyyyyyyyyyyyyy';                           // SMTP password
+$mail->Username = 'teste@systemcall.info';                 // SMTP username
+$mail->Password = ',F2IM{WR#s7a';                           // SMTP password
 $mail->SMTPSecure = 'ssl';                            // Enable encryption, 'ssl' also accepted
-//$mail->SMTPDebug = 1;
+$mail->SMTPDebug = 1;
 
-$mail->From = 'yyyyyyyyyyy';
+$mail->From = 'teste@systemcall.info';
 $mail->FromName = 'Ctracker';
 
 ?>
